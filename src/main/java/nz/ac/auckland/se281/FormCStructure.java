a76d2b7e961712f1ee2c2c5c2b09ff202fd7c946
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-28 09:26:17 +1200

    Create method which splits the country line and insert them into HashMap data structure.

---
+++ src/main/java/nz/ac/auckland/se281/FormCStructure.java
@@ -0,0 +1,23 @@
+package nz.ac.auckland.se281;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class FormCStructure {
+  private HashMap<String, Country> Countries;
+
+  public FormCStructure() {
+    Countries = new HashMap<>();
+  }
+
+  public HashMap<String, Country> FormStructure(List<String> countries) {
+    for (String CountriesLine : countries) {
+      String[] splited = CountriesLine.split(",");
+      String country = splited[0];
+      String continent = splited[1];
+      int tax = Integer.parseInt(splited[2]);
+      Countries.put(country, new Country(country, continent, tax));
+    }
+    return Countries;
+  }
+}
--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 /** This class is the main entry point. */
 public class MapEngine {
-  private HashMap<String, Country> Countries;
+
   private HashMap<String, List<String>> adjacencyList;
 
   public MapEngine() {
     // add other code here if you want
     adjacencyList = new HashMap<>();
+    Countries = new HashMap<>();
     loadMap(); // keep this mehtod invocation
   }
 
@@ -18,15 +19,10 @@ public class MapEngine {
   private void loadMap() {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
-    for (String CountriesLine : countries) {
-      String[] splited = CountriesLine.split(",");
-      String country = splited[0];
-      String continent = splited[1];
-      int tax = Integer.parseInt(splited[2]);
-      Countries.put(country, new Country(country, continent, tax));
-    }
 
-    // add code here to create your data structures
+    for (String adjacentCountriesLines : adjacencies) {
+      String[] splited = adjacentCountriesLines.split(",");
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
