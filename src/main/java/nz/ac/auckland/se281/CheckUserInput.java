6d05b4f86ca8832d3446c8b0ae64e6a569a31541
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-28 10:41:01 +1200

    Refactor CheckUserInput class to handle user input and display country information

--- src/main/java/nz/ac/auckland/se281/CheckUserInput.java
+++ src/main/java/nz/ac/auckland/se281/CheckUserInput.java
@@ -3,12 +3,18 @@ package nz.ac.auckland.se281;
 import java.util.Map;
 
 public class CheckUserInput {
-  public boolean exception(Map<String, Country> countries, String userInput) {
+  String name;
+  String continent;
+  String tax;
 
+  public void exception(Map<String, Country> countries, String userInput) {
     if (!countries.containsKey(userInput)) {
       throw new CountryNotFoundException();
     } else {
-      return true;
+      name = countries.get(userInput).getName();
+      continent = countries.get(userInput).getContinent();
+      tax = countries.get(userInput).getTax() + "";
+      MessageCli.COUNTRY_INFO.printMessage(name, continent, tax);
     }
   }
 }
