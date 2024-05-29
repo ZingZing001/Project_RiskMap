157b6a170ae278a276623612685e2b77d24fd601
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-05-30 08:55:23 +1200

    Refactor FindRoute class to use a map of adjacent nodes for breadth-first traversal

--- src/main/java/nz/ac/auckland/se281/FindRoute.java
+++ src/main/java/nz/ac/auckland/se281/FindRoute.java
@@ -3,16 +3,22 @@ package nz.ac.auckland.se281;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 public class FindRoute {
+  private Map<String, List<String>> adjNodes;
 
-  public List<String> breathFirstTraversal(String root) {
+  public FindRoute(Map<String, List<String>> map) {
+    this.adjNodes = map;
+  }
+
+  public List<String> findR_BFS(String start, String des) {
     List<String> visited = new ArrayList<>();
     Queue<String> queue = new LinkedList<>();
-    queue.add(root);
-    visited.add(root);
-    while (!queue.isEmpty()) {
+    queue.add(start);
+    visited.add(start);
+    while (!queue.isEmpty() && !visited.contains(des)) {
       String node = queue.poll();
       for (String n : adjNodes.get(node)) {
         if (!visited.contains(n)) {
